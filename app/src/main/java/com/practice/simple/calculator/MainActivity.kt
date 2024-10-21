31d43ea0c878613a33451c9b6bd6005f2a7139fa
iuytiu0934 <iuytiu0934@gamil.com>
2024-10-21 18:19:36 +0900

    Feat: Specify app launch screen
    - Create 'MainActivity' and set it as the app's launch screen.
    - Add androidx.activity for Activity lifecycle management.
    - Add androidx.constraintlayout to improve UI layout.

---
+++ app/src/main/java/com/practice/simple/calculator/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.practice.simple.calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        enableEdgeToEdge()
+    }
+}
