902fbeac26ba643a2a61d1d20e7d5b9cb5d62656
iuytiu0934 <iuytiu0934@gamil.com>
2024-10-23 23:33:38 +0900

    Chore: Apply viewBinding

--- app/src/main/java/com/practice/simple/calculator/MainActivity.kt
+++ app/src/main/java/com/practice/simple/calculator/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.practice.simple.calculator
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.practice.simple.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
